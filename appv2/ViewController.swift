e436487ea2ab1e85253850d54b080011dd49bea4
sabrieker <ekersabri1050@hotmail.com>
2023-02-04 19:44:02 +0100

    Initial Commit

---
+++ appv2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appv2
+//
+//  Created by Sabri Samed Eker on 04.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
